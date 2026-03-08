4d9a25457ecb237eb0b4ac312cf3f08a40600bdd
Michael <101535883+michaeljborg@users.noreply.github.com>
2026-03-08 14:12:29 -0400

    Revert "Monitoring"

--- scripts/node_git.py
+++ scripts/node_git.py
@@ -2,16 +2,14 @@ import subprocess
 
 # Configuration
 NODES = ["node2", "node3", "node4", "node5"]
-BRANCH = "matt"
+BRANCH = "main"
 PROJECT_PATH = "/home/cluster/ELEC490-Capstone"
 
 def sync_node(node):
     print(f"--- Syncing {node} ---")
     # Command to run on the remote node
-    #remote_cmd = f"cd {PROJECT_PATH} && git fetch origin && git checkout {BRANCH} && git pull {BRANCH}"
+    remote_cmd = f"cd {PROJECT_PATH} && git fetch origin && git checkout {BRANCH} && git pull origin {BRANCH}"
     
-    # sometimes this command needs to be done
-    remote_cmd = f"cd {PROJECT_PATH} && git fetch origin && git checkout {BRANCH} && git reset --hard origin/{BRANCH} && git clean -fd"
     try:
         # Run via SSH
         result = subprocess.run(
